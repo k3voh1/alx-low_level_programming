acac2b5ac02ae81e086f45f9080a069fcd746933
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-10 11:51:02 +0000

    task 4 corr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
  *main - Entry point
 
  * Return: Always 0 (success)
- * */
+ * 
+ */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 
