e033f5572fb441dd7a31ee0bab222326990cffd8
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 13:53:35 +0000

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- *main - print lowercase a-z
+ *main - Entry point
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int ones;
-	int tens;
-	int hunds;
 	int thous;
+	int hunds;
+	int tens;
+	int ones;
 
 	for (thous = '0'; thous <= '9'; thous++)
 	{
 		for (hunds = '0'; hunds <= '9'; hunds++)
 		{
-			for (tens = thous ; tens <= '9'; tens++)
+			for (tens = thous; tens <= '9'; tens++)
 			{
 				for (ones = hunds + 1; ones <= '9'; ones++)
 				{
@@ -25,7 +25,7 @@ int main(void)
 					putchar(' ');
 					putchar(tens);
 					putchar(ones);
-					if (!((thous == '9' && hunds == '8') && (tens == '9' && ones == '9')))
+					if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
@@ -36,5 +36,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
