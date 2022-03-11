97d84627adb276c1d91f6ef86c3e0ac490d0a22d
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 12:47:31 +0000

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int hunds;
+
+	for (hunds = '0'; hunds <= '9'; hunds++)
+	{
+		for (tens = (hunds + 1); tens <= '9'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++)
+			{
+				putchar(hunds);
+				putchar(tens);
+				putchar(ones);
+			}
+		}
+		if (hunds != '7' && tens != '8' && ones != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
