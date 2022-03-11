947c18f5356b29e4bd8b9998afb1210b8e6fc058
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 12:33:04 +0000

    Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,13 +16,10 @@ int main(void)
 		{
 			putchar(tens);
 			putchar(ones);
-			if (tens != '8')
+			if ((tens != '8') && (ones != '9'))
 			{
-				if (ones != '9')
-				{
 				putchar(',');
 				putchar(' ');
-				}
 			}
 		}
 	}
