7988944697f0b25c4439c8335abf4f06696ab5a5
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 11:31:41 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hexint;
+	char hexalp;
+
+	for (hexint = 0; hexint <= '9' ; hexint++)
+	{
+		putchar(hexint);
+	}
+
+	for (hexalp = 'a'; hexalp <= 'f'; hexalp++)
+	{
+		putchar(hexalp);
+	} 
+	putchar('\n');
+
+	return (0);
+}
