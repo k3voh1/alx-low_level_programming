4a8072aeed25f80f68516f73bc968cc54bb97be5
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 12:16:52 +0000

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = (tens + '1'); ones <= '9'; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
-			if (tens != 9 && ones != 9)
+			if (tens != '8' && ones != '9')
 			{
 				putchar(',');
 				putchar(' ');
