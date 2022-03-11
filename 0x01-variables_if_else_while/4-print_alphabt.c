86a8857f3a7c5bbf0967704d49ade964bb367a93
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 10:41:46 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+	alp = 'a';
+	
+	while (alp != 'q' && alp !='e')
+	{
+		putchar(alp);
+		alp++;
+
+	}
+	puchar('\n');
+
+	return (0);
+}
