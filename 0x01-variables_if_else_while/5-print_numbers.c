763c0d71f3ed4e48b31bc86522fc742665975a02
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 10:54:48 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <=9 ; num++)
+	{
+		putchar(num);
+
+	}
+	putchar('\n');
+}
