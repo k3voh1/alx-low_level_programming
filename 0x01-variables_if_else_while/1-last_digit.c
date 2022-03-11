f87263da83c884d2d0f1b82cd741444a7e56290d
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 09:57:01 +0000

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - Entry point
- *return: Always 0 (success)
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 
 int main(void)
