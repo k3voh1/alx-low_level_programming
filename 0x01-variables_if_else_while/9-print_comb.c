d4168c74463d4b232da3cd4e8ba54e52b484dbaa
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 11:48:20 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != 9)
+		{
+			putchar(num);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
