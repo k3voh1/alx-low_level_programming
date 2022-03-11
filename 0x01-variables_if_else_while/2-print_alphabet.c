aacebdad3107241041218886f63b46c1065fd64f
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-11 10:08:46 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower case letters
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+char alp = 'a';
+while (alp =< 26)
+{
+	putchar(alp);
+	alp++;
+}
+putchar("\n")
+}
