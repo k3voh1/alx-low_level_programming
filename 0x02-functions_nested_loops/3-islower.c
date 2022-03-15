81a70a3a45040662734ad87fcf5fca5661753d65
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-15 14:46:18 +0000

    test 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *int _islower(int c) - prints 10 times the alphabet in lowercase
+ *
+ *return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
