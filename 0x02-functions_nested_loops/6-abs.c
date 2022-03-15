8db5643776ab5e91ecd104ef55228021c4361606
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-15 15:44:05 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_abs - prints the sign of a number
+ *@n: number to be checked
+ *Return: absolute value
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	return n;
+}
