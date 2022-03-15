9f7799d5f88fe715d3fb752ac958bebbf57ce8e8
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-15 23:09:10 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers and returns the result
+ *@x: two integer arguments
+ *@y: twi integer arguments
+ *
+ *Return: sum of the two integers
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+	return(sum)
+}
