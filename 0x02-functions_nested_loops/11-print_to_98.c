a66255f249827a6595b720388da5a709105c0956
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-15 23:34:37 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (; n < 98; n++)
 		{
 			printf("%d ,", n);
 		}
 	}
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (; n > 98; n--)
 		{
