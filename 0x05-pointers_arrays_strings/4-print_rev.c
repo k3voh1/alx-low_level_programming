96bcc86abf80e747187b7adc6607d1fdbe175f79
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-21 16:02:35 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	while (*(s + len)--)
+	{
+		_putchar(*(s + len));
+	}
+
+	_putchar('\n');
+}
