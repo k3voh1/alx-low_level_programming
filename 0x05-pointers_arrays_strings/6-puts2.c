2131dfad3b69508b1f7c8d14fa263402fa223682
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-21 16:59:06 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: string to be printed
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int x;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	for (x = 0; x <= len; x+=2)
+	{
+		_putchar(*(str + x));
+	}
+	_putchar('\n');
+}
