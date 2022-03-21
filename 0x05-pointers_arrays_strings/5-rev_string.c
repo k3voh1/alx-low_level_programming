4d552e7a1dfac8f531cb6fed137bdbcf7a347745
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-21 16:43:56 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int x = 0;
+	int tmp;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	while (len--)
+	{
+		tmp = s[x];
+		s[x] = s[len];
+		s[len] = tmp;
+		x++;
+	}
+}
