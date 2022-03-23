66c5f672329e4904296523344e7a7d82860f4c90
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-23 12:28:19 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (*(dest + len) != '\0')
+	{
+		len++;
+	}
+	while (len2 >= 0)
+	{
+		*(dest + len) = *(src + len2);
+		if (*(src + len2) == '\0')
+			break;
+		len2++;
+		len++;
+	}
+	return(dest);
+}
