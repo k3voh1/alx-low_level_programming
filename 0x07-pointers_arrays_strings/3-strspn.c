6e4cbb4c0331ff88c969870a0b56c0135c757936
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-28 11:38:52 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 	unsigned int accp = 0;
 
-	while (*(s + i) != '\0')
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
@@ -23,10 +23,9 @@ unsigned int _strspn(char *s, char *accept)
 			}
 			if (*(s + i) == '\0' && *(s + i) != *(accept + j))
 			{
-				return (accp);
+				break;
 			}
 		}
-		i++;
 	}
 	return (accp);
 }
