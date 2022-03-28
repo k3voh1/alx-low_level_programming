284030d30393c57e846ec7a566068e66277f7ee7
k3voh1 <kelvinduke.kj@gmail.com>
2022-03-28 11:00:09 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	while (*(s + i) != '\0')
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
 		else
-			return (0);
+			return ('\0');
 		i++;
 	}
 }
