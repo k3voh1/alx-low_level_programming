adafc37cc6aeba0ac93596818b814183b3e48f9c
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 13:37:00 +0000

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,19 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	int i, cents;
+	int cents, ncoins = 0;
 
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argc[1]);
 
-	if (cents < 0)
-	{
-		printf("0\n");
-	}
+	cents = atoi(argv[1]);
+
 	while (cents > 0)
 	{
 		if (cents >= 25)
@@ -34,12 +31,10 @@ int main(int argc, char *argv[])
 			cents -= 5;
 		else if (cents >= 2)
 			cents -= 2;
-		else
-		{
+		else if (cents >= 1)
 			cents -= 1;
-		}
-		i += 1;
+		ncoins += 1;
 	}
-	printf("%d\n", i);
+	printf("%d\n", ncoins);
 	return (0);
 }
