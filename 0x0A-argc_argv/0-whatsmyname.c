98df07153b172544844ae5059e28245809a2d73b
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 11:58:59 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * main - prints name of the program
  * @argc: args counter
  * @argv: pointer to array of arguments
- * Return: 0 on successs
+ * Return: 0 on success
  */
 
-int main(int argc, int *argv[])
+int main(int argc __attribute__((unused)), int *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
