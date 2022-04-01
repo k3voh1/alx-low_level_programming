05997fa3764f010b8adae7f8ce943df22012c429
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 12:09:44 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - prints the number of args passed onto it
  * @argc: number of args
  * @argv: array that contains the program args
  * Return: 0 on success
  */
-
-int main (int argc, int *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
-	return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
