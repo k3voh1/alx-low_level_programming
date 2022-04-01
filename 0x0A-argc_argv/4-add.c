52101fbd65e0e96dae6fa2492de6aadecbdfea12
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 13:05:01 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,20 +18,18 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
+		if (!isdigit(argv[i]))
 		{
-			if (!isdigit(argv[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
 		}
 	}
 	printf("%d\n", sum);
