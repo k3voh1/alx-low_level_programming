5df34bac911b91f8d0eedc05e2d222118b70a2c0
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 12:06:08 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of args passed onto it
+ * @argc: number of args
+ * @argv: array that contains the program args
+ * Return: 0 on success
+ */
+
+int main (int argc, )
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
