b1e4505d12ef1074a38cf9de76e7a73fb440b404
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-01 11:54:31 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints name of the program
+ * @argc: args counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on successs
+ */
+
+int main(int argc, int *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
