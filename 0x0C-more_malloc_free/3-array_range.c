5c641ffc7eef85341fd3d1627cbf8db370971e20
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-07 21:20:30 +0000

    task 3 corr 1

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	x = (max - min + 1);
 	array = malloc(sizeof(int) * x);
-	if (array = NULL)
+	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < x; i++)
 	{
