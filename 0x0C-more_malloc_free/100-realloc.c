19e2c2f1cdc4a33be05856bc9fe1400e66971f2a
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-08 00:52:43 +0000

    task 100 corr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,26 +2,44 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * with a call to malloc: malloc(old_size)
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: size, in bytes of the new memory block
- * Return: pointer to the newly allocated size
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
+ * Return: pointer to reallocated memory
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-	if (new_size == 0 || ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	free(ptr);
-	ptr = malloc(new_size);
-	return (ptr);
+void *p;
+unsigned int i;
+
+if (new_size == 0 && ptr != NULL) /* free memory if reallocate 0 */
+{
+free(ptr);
+return (NULL);
+}
+
+if (new_size == old_size) /* return ptr if reallocating same old size */
+return (ptr);
+
+if (ptr == NULL) /* malloc new size if ptr is originally null */
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+else
+return (p);
+}
+
+p = malloc(new_size); /* malloc and check error */
+if (p == NULL)
+return (NULL);
+
+/* fill up values up till minimum of old or new size */
+for (i = 0; i < old_size && i < new_size; i++)
+*((char *)p + i) = *((char *)ptr + i);
+free(ptr); /* free old ptr */
+
+return (p);
 }
