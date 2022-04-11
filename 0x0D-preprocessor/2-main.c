9deeeedf9ca85bd98a2f8ddeda7a452f3667b4a9
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-11 09:30:13 +0000

    task 2 corr

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-void main(void)
+/**
+ * main - prints name of current file
+ * Return: 0 on success
+ */
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
