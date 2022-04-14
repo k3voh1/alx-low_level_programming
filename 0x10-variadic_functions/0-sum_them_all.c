4efeb37e9bc6d7b57b467402c56046195832cb19
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-14 11:06:51 +0000

    task 0 add stdarg headerfile

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 #include <stdlib.h>
-#include <stdarg.h>
 
 /**
  * sum_them_all -  returns the sum of all its parameters
@@ -13,13 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
+	va_list list;
 
 	if (n == 0)
 		return (0);
-	va_list list;
-
 	va_start(list, n);
-
 	for (i = 0; i < n; i++)
 	{
 		sum += va_args(list, int);
